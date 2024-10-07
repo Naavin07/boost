488c796e9dd0c978d537739fdf6c2f2f80404760
Naavin07 <naavinbalayah@gmail.com>
2024-10-08 01:07:38 +0800

    Added SpecialOfferRepository, SpecialOffer entity

---
+++ src/main/java/com/example/boost/repository/SpecialOfferRepository.java
@@ -0,0 +1,7 @@
+package com.example.boost.repository;
+
+import com.example.boost.entity.SpecialOffer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpecialOfferRepository extends JpaRepository<SpecialOffer, Long> {
+}
--- src/main/java/com/example/boost/service/VoucherService.java
+++ src/main/java/com/example/boost/service/VoucherService.java
@@ -1,12 +1,9 @@
 package com.example.boost.service;
 
-import com.example.boost.repository.RecipientRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VoucherService {
 
-    @Autowired
-    RecipientRepository recipientRepository;
+
 }
